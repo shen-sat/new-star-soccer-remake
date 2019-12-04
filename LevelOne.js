45c196234110148076d23eff9901b9a66c56c343
Shen Satkunarasa <shensatkunarasa@Shen-Satkunarasas-MacBook-Pro.local>
2019-12-04 18:59:31 +0000

    Add arrow

--- LevelOne.js
+++ LevelOne.js
@@ -7,6 +7,7 @@ class LevelOne extends Phaser.Scene {
 	preload() {
 		this.load.image('pitch', 'assets/pitch.png');
 		this.load.image('ball', 'assets/square.png');
+		this.load.image('arrow', 'assets/arrow.png');
 		
 		
 	}
@@ -15,13 +16,22 @@ class LevelOne extends Phaser.Scene {
 		this.pitch = this.physics.add.image(this.cameras.main.width/2, this.cameras.main.height/2, 'pitch');
 		this.ball = this.physics.add.image(this.cameras.main.width/2 + 50, this.cameras.main.height/2 + 50, 'ball');
 		this.ball.setDrag(20, 0)
+		this.arrow = this.physics.add.image(this.cameras.main.width/2, this.cameras.main.height/2, 'arrow');
+		this.arrowScale = 0.035;
+		this.arrow.setScale(this.arrowScale);
 		
 		this.kickButton = this.input.keyboard.addKey('A'); 
+		this.arrow.anchor.setTo(this0.5 * 0.035, 0.5)
 	}
 
 	update() {
 		if (this.justPressed(this.kickButton)) { this.ball.setVelocityX(50) }
+
+		this.arrow.setX(this.ball.x + (this.arrow.width/2 * this.arrowScale));
+		this.arrow.setY(this.ball.y);
 	}
+	
+
 
 	justPressed(key) {
 		if (Phaser.Input.Keyboard.JustDown(key)) { return true }
