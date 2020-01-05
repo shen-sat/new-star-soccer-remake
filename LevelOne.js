f645b71892ddbb9b010ea3fe2161ad728e604d86
Shen Sat <shensat@shens-mbp.home>
2020-01-05 17:32:24 +0000

    Finish ball curving

--- LevelOne.js
+++ LevelOne.js
@@ -6,7 +6,6 @@ class LevelOne extends Phaser.Scene {
 
 	preload() {
 		this.load.image('pitch', 'assets/sensible-pitch-bw-no-line.png');
-		// this.load.image('ball', 'assets/square.png');
 		this.load.image('arrow', 'assets/line-v3.png');
 		this.load.image('post-left', 'assets/goal-post-left.png');
 		this.load.image('post-right', 'assets/goal-post-right.png');
@@ -15,40 +14,46 @@ class LevelOne extends Phaser.Scene {
 	}
 
 	create() {
+		//Pitch
 		this.pitch = this.physics.add.image(this.cameras.main.width/2, this.cameras.main.height - 120, 'pitch');
+		//Arrow
 		this.arrow = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2, 'arrow');
 		this.arrowScale = 1.5;
 		this.arrow.setScale(this.arrowScale);
 		this.arrow.setOrigin(0,0.5);
+		this.arrow.rotation = this.arrow.rotation - 1.570796;
+		this.arrowStartingScale = this.arrowScale;
+		this.arrowScaleXMax = 3.5;
+		//Ball
 		this.ball = this.physics.add.sprite(this.cameras.main.width/2 + 50, this.cameras.main.height/2 + 50, 'ball');
 		this.ball.setDamping(true);
 		this.ball.setDrag(0.98)
-		
-
-		this.keys = this.input.keyboard.createCursorKeys();
-		this.kickButton = this.input.keyboard.addKey('A');
 		this.ballSpeed = 150;
-
-		this.arrowStartingScale = this.arrowScale;
-		this.arrowScaleXMax = 3.5;
 		this.ballStartingSpeed = this.ballSpeed;
 		this.ballMaxSpeed = this.ballSpeed * 3;
-
 		this.ballKickable = true;
-
-		this.posts = this.physics.add.staticGroup();
-
+		this.ball.setBounce(0.75,0.75);
+		this.ballDirection = 0;
+		this.curlUnit = 0.00001745329001 * 1.25
+		this.curlMax = this.curlUnit * 200
+		this.ballCurl = 0
+		this.savedBallCurl = 0;
+		//Goal
 		this.postLeft = this.physics.add.staticImage(530, 209, 'post-left');
 		this.postRight = this.physics.add.staticImage(672, 209, 'post-right');
 		this.netBoundary = this.physics.add.staticImage(this.cameras.main.width/2 - 2, this.cameras.main.height/2 - 244, 'net-boundary');
-
+		this.posts = this.physics.add.staticGroup();
 		this.posts.add(this.postLeft)
 		this.posts.add(this.postRight)
-		
-		this.ball.setBounce(0.75,0.75);
+		//Buttons
+		this.keys = this.input.keyboard.createCursorKeys();
+		this.kickButton = this.input.keyboard.addKey('A');
+		this.curlLeftButton = this.input.keyboard.addKey('O');
+		this.curlRightButton = this.input.keyboard.addKey('P');
+		//Colliders
 		this.physics.add.collider(this.ball, this.posts);
 		this.physics.add.collider(this.ball, this.netBoundary, this.stopBall, null, this);
-
+		//Animations
 		this.anims.create({
 			key: 'roll',
 			frames: this.anims.generateFrameNumbers('ball', { frames: [0,1,2,3,4,5,6,7,8,9]}),
@@ -56,47 +61,37 @@ class LevelOne extends Phaser.Scene {
 			repeat: -1
 		});
 
-		this.ballReadyToCurl = false;
-		this.startingDirection = 0;
+		
 	}
 
 	update() {
-		//reset
+		//Attach arrow to ball
 		this.arrow.setX(this.ball.x);
 		this.arrow.setY(this.ball.y);
-
+		//Get bal to face arrow direction
+		this.ball.rotation = this.arrow.rotation + 1.570796;  //90 degrees in radians
+		//Button actions
 		if (this.keys.right.isDown) {
 			this.arrow.rotation += 0.05
 		} else if (this.keys.left.isDown) {
 			this.arrow.rotation -= 0.05
 		}
-
-		this.ball.rotation = this.arrow.rotation + 1.570796;  //90 degrees in radians
-
 		if (this.keys.up.isDown) {
 			this.arrow.scaleX = Math.min(this.arrow.scaleX + 0.02, this.arrowScaleXMax)
 			this.ballSpeed = this.ballStartingSpeed + ((this.ballMaxSpeed - this.ballStartingSpeed)/(this.arrowScaleXMax - this.arrowStartingScale)) * (this.arrow.scaleX - this.arrowStartingScale)
-
 		}
-
-		if (this.ball.body.speed < this.ballSpeed * 0.025) {
-			this.ball.body.setVelocity(0)
-
+		if (this.curlLeftButton.isDown) {
+			this.ballCurl = Math.min(this.curlMax, this.ballCurl + this.curlUnit)
 		}
-
-		if (this.ballReadyToCurl) {
-			console.log(this.ball.body.speed) 
-			this.startingDirection += 0.01745329
-			this.physics.velocityFromRotation(this.startingDirection, this.ball.body.speed, this.ball.body.velocity) 
+		if (this.curlRightButton.isDown) {
+			this.ballCurl = Math.max(-this.curlMax, this.ballCurl - this.curlUnit)
 		}
-
-		if (this.justPressed(this.kickButton) && this.ballKickable) {  
-			this.physics.velocityFromRotation(this.arrow.rotation, this.ballSpeed, this.ball.body.velocity);
-			this.ballReadyToCurl = true;
-			this.startingDirection = this.arrow.rotation;	
-		}
-
-
+		//Adjust ball stop speed
+		this.speedUpBallStop()
+		//Apply curl
+		this.ballDirection += this.savedBallCurl 
+		this.physics.velocityFromRotation(this.ballDirection, this.ball.body.speed, this.ball.body.velocity) 
+		//Ball start and stop actions
 		if (this.ball.body.speed > 0) {
 			this.ballKickable = false;
 			this.arrow.setAlpha(0);
@@ -104,13 +99,18 @@ class LevelOne extends Phaser.Scene {
 			this.ballSpeed = this.ballStartingSpeed
 			this.ball.play('roll', true);
 			this.ball.anims.setTimeScale(this.ball.body.speed/50); //this will have to be adjusted depending on 'feel' of roll
-			// this.ball.rotation += 0.1745329
 		} else {
 			this.ball.anims.stop()
 			this.ballKickable = true
 			this.arrow.setAlpha(1);
 		}
-
+		//Apply kick
+		if (this.justPressed(this.kickButton) && this.ballKickable) {  
+			this.physics.velocityFromRotation(this.arrow.rotation, this.ballSpeed, this.ball.body.velocity);
+			this.ballDirection = this.arrow.rotation; //For curl
+			this.savedBallCurl = this.ballCurl; //save curl so it can be used in next loops
+			this.ballCurl = 0; //reset curl for next kick
+		}
 		
 	}
 	
@@ -121,6 +121,12 @@ class LevelOne extends Phaser.Scene {
 	stopBall() {
 		this.ball.body.setVelocity(0)
 
+	}
+
+	speedUpBallStop() {
+		if (this.ball.body.speed < this.ballSpeed * 0.025) {
+			this.stopBall()
+		}
 	}
 
 }
